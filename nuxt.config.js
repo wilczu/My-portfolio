05da305389d570d56c5fc61dd3958e6ac5755e7c
Wilchu <kacpersoja1@gmail.com>
2023-03-31 00:03:29 +0100

    add tailwindcss to project

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,6 +24,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@/assets/css/main.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -43,5 +44,13 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
   }
 }
