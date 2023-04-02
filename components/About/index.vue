b46547514560dc5732f2d9ebf128b2d520bb67a9
Wilchu <kacpersoja1@gmail.com>
2023-04-02 20:07:00 +0100

    Add tech stack and new design to about me section

--- components/About/index.vue
+++ components/About/index.vue
@@ -10,26 +10,72 @@
         <section class="mt-12">
             <div class="grid grid-cols-4 gap-12">
 
-                <div class="col-span-4 md:col-span-1">
+                <div class="col-span-4 md:col-span-1 relative">
+                    <div class="absolute bg-pink-600 rounded-lg top-0 left-0 w-4/5 h-4/5 z-10"></div>
                     <img 
-                        src="https://i.pinimg.com/564x/97/a5/50/97a55002318c28bcdf651c3862198c22.jpg"
+                        src="https://media.licdn.com/dms/image/C4D03AQFIPo07g074bw/profile-displayphoto-shrink_800_800/0/1645010672296?e=1686182400&v=beta&t=_Ksrn5zJpKFnpiTZ3PDJd68UaNFl6Ii6zZ72oPRa1xY"
                         width="500"
                         height="350"
-                        class="w-[800px] h-[350px] object-cover border-l-8 border-t-8 border-pink-600"
+                        class="w-[800px] h-[350px] object-cover border-l-[22px] border-t-[22px] border-transparent z-20 relative"
                     />
                 </div>
-                
-                <article class="text-white mx-auto col-span-4 md:col-span-3">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ipsa ea obcaecati voluptates voluptatum! 
-                    Doloremque earum magni ipsum, iste rerum dicta corporis dolorem mollitia ipsa similique nostrum. Et, 
-                    quisquam eius.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ipsa ea obcaecati voluptates voluptatum! 
-                    Doloremque earum magni ipsum, iste rerum dicta corporis dolorem mollitia ipsa similique nostrum. Et, 
-                    quisquam eius.
-                </article>
+
+                <section class="mx-auto col-span-4 md:col-span-3 text-left">
+
+                    <header>
+                        <h3 class="text-pink-600 mb-2 text-lg">Hi again,</h3>
+                    </header>
+                    <article class="text-white">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ipsa ea obcaecati voluptates voluptatum! 
+                        Doloremque earum magni ipsum, iste rerum dicta corporis dolorem mollitia ipsa similique nostrum. Et, 
+                        quisquam eius.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ipsa ea obcaecati voluptates voluptatum! 
+                        Doloremque earum magni ipsum, iste rerum dicta corporis dolorem mollitia ipsa similique nostrum. Et, 
+                        quisquam eius.
+                    </article>
+
+                    <header>
+                        <h3 class="text-pink-600 my-2 text-lg">I'm writing in:</h3>
+                    </header>
+
+                    <section>
+                        <div class="flex gap-4">
+                            <Badge v-for="language in techStack" :key="language.id">
+                                <i :class="[language.icon, 'fa-2xl']"></i>
+                            </Badge>
+                        </div>
+                    </section>
+
+                </section>
 
             </div>
         </section>
 
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            techStack: [
+                {
+                    id: 1,
+                    icon: 'fa-brands fa-vuejs',
+                    name: 'Vue'
+                },
+                {
+                    id: 2,
+                    icon: 'fa-brands fa-html5',
+                    name: 'Html'
+                },
+                {
+                    id: 3,
+                    icon: 'fa-brands fa-css3-alt',
+                    name: 'Css'
+                }
+            ]
+        }
+    }
+}
+</script>
