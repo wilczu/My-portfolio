1dc019b09cd6a34bfe22e68065b8d572d00b9ff9
Wilchu <kacpersoja1@gmail.com>
2023-04-02 15:27:58 +0100

    Add basic about component

---
+++ components/About/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="text-center mx-auto container">
+        <div>
+            <h1 class="text-5xl text-white text-center">
+                <span class="text-pink-600">01.</span>
+                About me
+            </h1>
+        </div>
+
+        <section class="mt-12">
+            <div class="grid grid-cols-4 gap-12">
+
+                <div class="col-span-4 md:col-span-1">
+                    <img 
+                        src="https://i.pinimg.com/564x/97/a5/50/97a55002318c28bcdf651c3862198c22.jpg"
+                        width="500"
+                        height="350"
+                        class="w-[800px] h-[350px] object-cover border-l-8 border-t-8 border-pink-600"
+                    />
+                </div>
+                
+                <article class="text-white mx-auto col-span-4 md:col-span-3">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ipsa ea obcaecati voluptates voluptatum! 
+                    Doloremque earum magni ipsum, iste rerum dicta corporis dolorem mollitia ipsa similique nostrum. Et, 
+                    quisquam eius.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ipsa ea obcaecati voluptates voluptatum! 
+                    Doloremque earum magni ipsum, iste rerum dicta corporis dolorem mollitia ipsa similique nostrum. Et, 
+                    quisquam eius.
+                </article>
+
+            </div>
+        </section>
+
+    </div>
+</template>
