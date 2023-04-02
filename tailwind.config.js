57a92b800df7f404276867968f05b6858c2f5b98
Wilchu <kacpersoja1@gmail.com>
2023-04-02 20:06:26 +0100

    add my profile picture and new container size

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,18 @@ module.exports = {
     "./nuxt.config.{js,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'my-image': "url('https://i.pinimg.com/564x/22/b8/37/22b837ce4b35520a026a666295bf0f4e.jpg')",
+      },
+      screens: {
+        sm: '600px',
+        md: '728px',
+        lg: '984px',
+        xl: '1240px',
+        '2xl': '1496px',
+      },
+    },
   },
   plugins: [],
 }
