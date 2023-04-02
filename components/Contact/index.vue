6a81177fe123afaada6f4bfc5c085e10ff92e849
Wilchu <kacpersoja1@gmail.com>
2023-04-02 17:32:51 +0100

    Add contact me section

---
+++ components/Contact/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1 class="text-5xl text-white text-center">
+            <span class="text-pink-600">04.</span>
+            Contact
+        </h1>
+
+        <socialMedia class="py-6" />
+
+        <article class="text-white text-center container mx-auto w-1/2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. A pariatur expedita illum reprehenderit, 
+            voluptatibus quaerat delectus, incidunt repellat labore rem iure maxime molestias eveniet, fugit 
+            obcaecati error consequatur doloribus dolorem?
+        </article>
+
+        <section class="py-6">
+            <Button type="mail" name="Contact me" link="contact@wilchu.net" />
+        </section>
+
+    </div>
+</template>
