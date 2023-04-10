693da66eae0014c9ea0611bdf0ca272bdc7a0330
Wilchu <kacpersoja1@gmail.com>
2023-04-10 23:34:06 +0100

    Add hero section

---
+++ components/Hero/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container mx-auto pb-44 pt-24">
+        <header class="text-white">
+        
+        <h1 class="text-5xl">
+            Hi there, my name is
+            <span class="text-pink-600">Kacper 👋</span>
+        </h1>
+
+        <h3 class="text-3xl mt-2">I'm a web developer</h3>
+
+        <article class="mt-2">
+            My focus is on creating visually appealing, user-friendly websites that provide a <br />
+            seamless user experience. I use my expertise in HTML, CSS, JavaScript, and other <br />
+            relevant technologies to develop responsive and mobile-friendly interfaces <br />
+        </article>
+
+
+        
+    </header>
+    </div>
+</template>
