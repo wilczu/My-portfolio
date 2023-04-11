355601b111e983a41fb78379a29bd63c7d39ac70
Wilchu <kacpersoja1@gmail.com>
2023-04-11 21:56:12 +0100

    add main page with components

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <Tutorial/>
+  <div>
+    <main class="bg-gray-800 px-8 md:px-16">
+      <Navigation/>
+      <Hero />
+      <About class="py-16" />
+      <Projects class="py-16" />
+      <Experience class="py-16"  />
+      <Contact class="py-16" />
+    </main>
+    <fixedSocials class="hidden md:block" />
+    <Footer />
+  </div>
 </template>
 
 <script>
