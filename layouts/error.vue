85b65a44262bfbda4dcf703f6e2716ed7979adde
Wilchu <kacpersoja1@gmail.com>
2023-04-16 19:06:18 +0100

    add 404 error page

---
+++ layouts/error.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        
+        <div class="flex items-center justify-center h-screen text-white text-center">
+            <div class="flex flex-col gap-4">
+                <div>
+                    <h1 class="text-pink-600 text-5xl">Not found</h1>
+                </div>
+                <div>
+                    <h3 class="text-2xl">This page doesn't exist</h3>
+                </div>
+                <div>
+                    <nuxt-link to="/">
+                        <Button type="button" name="Home page" />
+                    </nuxt-link>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
