0f13a007a78b213da8090c3d5956e931670ec157
Wilchu <kacpersoja1@gmail.com>
2023-04-16 17:02:53 +0100

    Add correct content to about me section

--- components/About/index.vue
+++ components/About/index.vue
@@ -25,23 +25,29 @@
                     <header>
                         <h3 class="text-pink-600 mb-2 text-lg">Hi again,</h3>
                     </header>
+
                     <article class="text-white">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ipsa ea obcaecati voluptates voluptatum! 
-                        Doloremque earum magni ipsum, iste rerum dicta corporis dolorem mollitia ipsa similique nostrum. Et, 
-                        quisquam eius.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ipsa ea obcaecati voluptates voluptatum! 
-                        Doloremque earum magni ipsum, iste rerum dicta corporis dolorem mollitia ipsa similique nostrum. Et, 
-                        quisquam eius.
+                        A little bit about myself. <br />
+                        I'm a web developer who's absolutely passionate about creating amazing websites. I'm a bit of a perfectionist when 
+                        it comes to design, so I always make sure that every pixel is in its right place. As a front-end developer, 
+                        I place a lot of emphasis on creating a great user experience. After all, that's what really matters to the 
+                        people who use the sites I build!
+                    </article>
+
+                    <article class="text-white mt-2">
+                        I just love creating cool stuff in the digital world. Whether it's making music, designing graphics, building websites, 
+                        or crafting 3D models and art, I'm always excited to dive in and see what I can come up with. There's just something 
+                        about the creative process that really gets me going, and I can't wait to see where it takes me next!
                     </article>
 
                     <header>
-                        <h3 class="text-pink-600 my-2 text-lg">I'm writing in:</h3>
+                        <h3 class="text-pink-600 my-2 text-lg">Technologies, libraries and tools I'm using:</h3>
                     </header>
 
                     <section>
-                        <div class="flex gap-4">
-                            <Badge v-for="language in techStack" :key="language.id">
-                                <i :class="[language.icon, 'fa-2xl']"></i>
+                        <div class="flex flex-wrap gap-4">
+                            <Badge v-for="language in techStack" :key="language.id" class="group-hover relative">
+                                <img :src="language.icon" class="w-100 h-8" />
                             </Badge>
                         </div>
                     </section>
@@ -61,18 +67,68 @@ export default {
             techStack: [
                 {
                     id: 1,
-                    icon: 'fa-brands fa-vuejs',
+                    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg',
                     name: 'Vue'
                 },
                 {
                     id: 2,
-                    icon: 'fa-brands fa-html5',
-                    name: 'Html'
+                    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nuxtjs/nuxtjs-original.svg',
+                    name: 'Nuxt'
                 },
                 {
                     id: 3,
-                    icon: 'fa-brands fa-css3-alt',
+                    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg',
                     name: 'Css'
+                },
+                {
+                    id: 4,
+                    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg',
+                    name: 'Tailwindcss'
+                },
+                {
+                    id: 5,
+                    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg',
+                    name: 'Html'
+                },
+                {
+                    id: 6,
+                    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg',
+                    name: 'Django'
+                },
+                {
+                    id: 7,
+                    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bitbucket/bitbucket-original.svg',
+                    name: 'Bitbucket'
+                },
+                {
+                    id: 8,
+                    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg',
+                    name: 'Docker'
+                },
+                {
+                    id: 9,
+                    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+                    name: 'JavaScript'
+                },
+                {
+                    id: 10,
+                    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jira/jira-original.svg',
+                    name: 'Jira'
+                },
+                {
+                    id: 11,
+                    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nginx/nginx-original.svg',
+                    name: 'Nginx'
+                },
+                {
+                    id: 12,
+                    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg',
+                    name: 'Npm'
+                },
+                {
+                    id: 13,
+                    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg',
+                    name: 'Postgresql'
                 }
             ]
         }
