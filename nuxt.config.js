cfcab221a6072f65a48120794c83e37c000acd0c
Wilchu <kacpersoja1@gmail.com>
2023-04-16 19:24:48 +0100

    add meta tags and favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,18 +7,19 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'portfolio',
+    title: 'Wilchu - portfolio',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { hid: 'description', name: 'description', content: "I'm a web developer who's absolutely passionate about creating amazing websites. I'm a bit of a perfectionist when it comes to design, so I always make sure that every pixel is in its right place." },
+      { name: 'format-detection', content: 'telephone=no' },
+      { name: 'keywords', content: 'wilchu, kacper soja, kacper, soja, web developer, frontend developer, wilchu github' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: 'https://res.cloudinary.com/dqswntnlm/image/upload/v1681669396/wilchu/favicon_gxlkb1.ico' }
     ],
     script: [
       {
