3eb417a4a3b932e2848898412e6ee254a5dfd97e
Wilchu <kacpersoja1@gmail.com>
2023-04-24 22:26:35 +0100

    add app.vue with content of main page

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    <Head>
+      <Title>Wilchu - portfolio</Title>
+    </Head>
+  
+    <div class="relative">
+      <main class="px-8 md:px-16">
+        <Navigation/>
+        <Hero />
+        <About class="py-16" />
+        <Projects class="py-16" />
+        <Experience class="py-16"  />
+        <Contact class="py-16" />
+      </main>
+      <fixedSocials class="hidden md:block" />
+      <Footer />
+    </div>
+</div>
+</template>
+  
